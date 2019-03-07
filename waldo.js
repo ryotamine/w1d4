ef86a2b9ac37d46642429d994b997caf1058786f
Ryota Mine <ryotamine@gmail.com>
2019-03-07 16:17:32 +0000

    for loop version

--- waldo.js
+++ waldo.js
@@ -1,14 +1,17 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
+  // loops array [0, 1, 2, 3]
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] === "Waldo") {
-      found();   // execute callback
+      found(i);   // execute callback
     }
   }
 }
 
-function actionWhenFound() {
-  console.log("Found him!");
+// change output to "Found Waldo at index 2!"
+function actionWhenFound(yes) {
+  console.log("Found Waldo at index " + yes + "!");
 }
 
+// findWaldo ([0, 1, 2, 3], actionWhenFound)
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
